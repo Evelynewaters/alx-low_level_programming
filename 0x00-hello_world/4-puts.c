f976e55fdb64cc981449ed2ad386ae122d2eeeb7
Evelynewaters <eveclaraify@gmail.com>
2023-06-16 03:40:59 -0700

    Docs- This commit contains a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-
-int main() {
+/**
+ * main - entry point 
+ *
+ * return 0 - success
+ * 
+ */
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
