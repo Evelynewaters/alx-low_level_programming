df306927410473a2b0f0640e981e90efcdc715a3
Evelynewaters <eveclaraify@gmail.com>
2023-06-16 04:03:31 -0700

    This commint contains changes made to file 100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
 /**
  * main - entry point 
  *
- * return 0 - success
- * 
+ * return: 0 (success) 
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
