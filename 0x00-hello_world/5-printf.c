85c538ce138b201192fe2f59fb96f75267c654ed
Evelynewaters <eveclaraify@gmail.com>
2023-06-18 03:21:33 -0700

    This commit has changes in 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entrypoint
  *
- * Return: Always 0(sucess)
+ * Return: 0(sucess)
  */
 int main(void)
 {
-	printf("/"with proper grammar, but the outcome is a piece of art, \n");
+	printf("/\"with proper grammar, but the outcome is a piece of art, \"\n");
 	return (0);
 }
