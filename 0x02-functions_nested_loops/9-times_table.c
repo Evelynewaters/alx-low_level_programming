5bdc0cfc9f9130c10c6e9de898802560b009bc5a
Evelynewaters <eveclaraify@gmail.com>
2023-06-21 14:19:25 -0700

    Fix- syntax errors fixed in file 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,17 +6,17 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++);
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
 
-			if (j == 0);
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
-			if (k < 10 && j != 0)
+			else if (k < 10 && j != 0)
 			{
 
 				_putchar(',');
@@ -24,7 +24,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(k + '0');
 			} 
-			else (k >= 10)
+			else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
