653f5d5d1a4b30fff3470d9641c754136e6edaf3
Evelynewaters <eveclaraify@gmail.com>
2023-06-21 14:33:10 -0700

    Fix- syntax errors fixed in file 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,22 +16,17 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-			else if (k < 10 && j != 0)
-			{
-
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} 
-			else if (k >= 10)
+			else 
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 10) + '0');
+				if (k < 10)
+					_putchar(' ');
+				else
+					_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
