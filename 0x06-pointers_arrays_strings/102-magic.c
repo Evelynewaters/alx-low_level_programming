c007993192d85d0583702bf8cb90d1247babc660
Evelynewaters <eveclaraify@gmail.com>
2023-06-30 10:57:45 -0700

    Fix- changes were made in file 102-magic.c to fix betty error

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,10 +10,8 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-	p = &a[2];
 	p = &n;
-	*(p + 5) = 98;
+	*(p + 6) = 98;
 	printf("a[2] = %d\n", a[2]);
-	printf("a[2] = %d\n", *(p + 3) - 4);
 	return (0);
 }
