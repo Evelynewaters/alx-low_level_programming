6633dbdbf1dde7c0205484de286c43969ba2fe34
Evelynewaters <eveclaraify@gmail.com>
2023-06-30 10:35:55 -0700

    Fix- changes were made in file 100-rot13.c to fix betty error

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
-char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 /**
  * rot13 - encoder rot13
  * @s: pointer to the string params
@@ -14,6 +12,8 @@ char *rot13(char *s)
 	int j;
 
 	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
