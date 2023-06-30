01f6c40c95e3cc1893a7f89df7c5a008c68618c0
Evelynewaters <eveclaraify@gmail.com>
2023-06-30 10:30:30 -0700

    Docs- A c programwas written into file 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+
+char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 /**
  * rot13 - encoder rot13
  * @s: pointer to the string params
@@ -10,12 +12,9 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ
-		abcdefghijklmnopqrstuvwxyz";
-	char datarot[] "NOPQRSTUVWXYZABCDEFGHIJKLM
-		nopqrstuvwxyzabcdefghijklm";
-	
-	for (i = 0; s[] != '\0'; i++)
+
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
