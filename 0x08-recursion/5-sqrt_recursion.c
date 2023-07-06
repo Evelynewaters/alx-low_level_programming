77aec98de1d06840028564510dba0dc5063e52da
Evelynewaters <eveclaraify@gmail.com>
2023-07-05 20:14:54 -0700

    Docs- A c program was written inside file 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int actual_sqrt_recursion (int n, int i);
+int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion - returns the natural squareroot of a number
  * @n: number to calculate the squareroot of
