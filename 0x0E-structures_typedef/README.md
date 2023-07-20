29b6f719c4ac6b467a4b8cacd784a00b7e3e03dd
Evelynewaters <eveclaraify@gmail.com>
2023-07-20 16:54:18 -0700

    Docs- This commit contains 11 c programs that solve tasks for 0x0E-structures_typedef project

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is the first readme file in 0x0E-structures_typedef directory
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include "dog.h"
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * struct dog - a structure representing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
