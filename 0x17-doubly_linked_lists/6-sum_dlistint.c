a4b0eea24b0472e77d9fcacb4d099f2d8aa7dd79
Evelynewaters <eveclaraify@gmail.com>
2023-11-09 22:57:12 -0800

    Docs- This commit contains solutions for task 6 and 7

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sums all the data (n) of dlistint_t list.
+ * @head: pointer
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
